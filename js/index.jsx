3c09dc21ad8635fdb23bd673f70c999c120c3b88
Sean Jun <seanjun21@gmail.com>
2016-07-20 08:31:09 -0700

    Added List and Board components

--- js/index.jsx
+++ js/index.jsx
@@ -1,41 +1,50 @@
 // jshint ignore: start
 
-var React = require('react');
-var ReactDOM = require('react-dom');
+var React = require( 'react' );
+var ReactDOM = require( 'react-dom' );
 
 var Card = function( props ) {
-    console.log( props, 'PROPS' );
-    return (
-        <div className="card">
-            <div className="card-text">{props.text}</div>
-        </div>
-    );
+  return (
+    <div className="card">{ props.text }</div>
+  );
 };
 
 var List = function( props ) {
-    props.cards = ["This is a text 1", "This is a text 2", "This is a text 3"];
-    var firstCard = props.cards[0];
-    <Card /> 
-    return (
-        <div className="list">
-            <div className="list-title">{props.title}</div>
-            <div className="card-list">{ cards }</div>
-        </div>
-    );
+  return (
+    <div className="list">
+      <div className="list-title">{ props.title }</div>
+      <div>{ props.cards }</div>
+    </div>
+  );
 };
 
 var Board = function() {
-    var list = [];
-    for ( var i = 0; i < 5; i++ ) {
-        list.push( <List /> );
-    }
-    return (
-        <div className="board">
-            { list }
-        </div>
-    );
-};
+  return (
+    <div className="board">
+      <div className="board-title">Board</div>
+      <div>
+        {[
+          <List title="List 1" cards={[ 
+            <Card text="Card 1"/>,
+            <Card text="Card 2"/>,
+            <Card text="Card 3"/>
+          ]}/>, 
+          <List title="List 2" cards={[ 
+            <Card text="Card 1"/>,
+            <Card text="Card 2"/>,
+            <Card text="Card 3"/>
+          ]}/>,
+          <List title="List 3" cards={[ 
+            <Card text="Card 1"/>,
+            <Card text="Card 2"/>,
+            <Card text="Card 3"/>
+          ]}/> 
+        ]}
+      </div>
+    </div>
+  )
+}
 
-document.addEventListener('DOMContentLoaded', function() {
-    ReactDOM.render(<Board />, document.getElementById('app'));
-});
+document.addEventListener( 'DOMContentLoaded', function() {
+  ReactDOM.render( < Board / > , document.getElementById( 'app' ) );
+} );
