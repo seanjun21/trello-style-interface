7da8297b8c7647412eff21ba22e8083adb5e1ddf
Eric Snell <eric_snell21@yahoo.com>
2016-07-20 16:58:32 -0400

    working on pushing to cards array on click

--- js/index.jsx
+++ js/index.jsx
@@ -3,49 +3,6 @@
 var React = require( 'react' );
 var ReactDOM = require( 'react-dom' );
 
-var ListContainer = React.createClass ({
-    getInitialState: function() {
-        return {
-            text: '',
-            cards: []
-        }
-    },
-    onAddInputChange: function() {
-        this.setState({value: event.target.value});
-    },
-    onAddClick: function() {
-        this.setState( {
-            clicked: true
-        } );
-        console.log(this.state);
-    },
-    render: function () {
-        return (
-            <List title={this.props.title} cards={this.props.cards} onAddClick={this.onAddClick} onAddInputChange={this.onAddInputChange} />
-        )
-    }
-});
-
-var Card = function( props ) {
-  return (
-    <div className="card">{ props.text }</div>
-  );
-};
-
-var List = React.createClass ({
-
-    render: function () {
-        return (
-            <div className="list">
-                <div className="list-title">{ this.props.title }</div>
-                <div>{ this.props.cards }</div>
-                <input type="text" onChange={ this.props.onAddInputChange }/>
-                <button onClick={ this.props.onAddClick }></button>
-            </div>
-        );
-    }
-});
-
 var Board = function() {
 
   return (
@@ -63,6 +20,50 @@ var Board = function() {
     </div>
   )
 }
+
+
+var ListContainer = React.createClass ({
+    getInitialState: function() {
+        return {
+            text: '',
+            cards: []
+        }
+    },
+    onAddInputChange: function(event) {
+        this.setState({value: event.target.value});
+    },
+    onAddClick: function() {
+        this.setState({this.state.cards.push(this.state.value)});
+    },
+    render: function () {
+        return (
+            <List title={this.props.title} cards={this.props.cards} onAddClick={this.onAddClick} onAddInputChange={this.onAddInputChange} />
+        )
+    }
+});
+
+
+var List = React.createClass ({
+
+    render: function () {
+        return (
+            <div className="list">
+                <div className="list-title">{ this.props.title }</div>
+                <div>{ this.props.cards }</div>
+                <input type="text" onChange={ this.props.onAddInputChange }/>
+                <button onClick={ this.props.onAddClick }></button>
+            </div>
+        );
+    }
+});
+
+
+var Card = function( props ) {
+  return (
+    <div className="card">{ props.text }</div>
+  );
+};
+
 
 document.addEventListener( 'DOMContentLoaded', function() {
   ReactDOM.render( < Board / > , document.getElementById( 'app' ) );
