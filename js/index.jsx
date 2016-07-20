735669542b19486fcf23ce12d5d7051c07f83109
Joe Seago <joe.seago@gmail.com>
2016-07-20 14:25:23 -0600

    added ListContainer stuff

--- js/index.jsx
+++ js/index.jsx
@@ -3,60 +3,61 @@
 var React = require( 'react' );
 var ReactDOM = require( 'react-dom' );
 
+var ListContainer = React.createClass ({
+    getInitialState: function() {
+        return {
+            text: '',
+            cards: []
+        }
+    },
+    onAddInputChange: function() {
+        this.setState({value: event.target.value});
+    },
+    onAddClick: function() {
+        this.setState( {
+            clicked: true
+        } );
+        console.log(this.state);
+    },
+    render: function () {
+        return (
+            <List title={this.props.title} cards={this.props.cards} onAddClick={this.onAddClick} onAddInputChange={this.onAddInputChange} />
+        )
+    }
+});
+
 var Card = function( props ) {
   return (
     <div className="card">{ props.text }</div>
   );
 };
 
-var List = React.createClass {
-  
-  render: function() {
-    return (
-      <div className="list">
-        <div className="list-title">{ props.title }</div>
-        <div>{ props.cards }</div>
-        <input type="text" onChange={ props.onAddInputChange }/>
-        <button onClick={ props.onAddClick }></button>
-      </div>
-    );
-  }
-};
+var List = React.createClass ({
+
+    render: function () {
+        return (
+            <div className="list">
+                <div className="list-title">{ this.props.title }</div>
+                <div>{ this.props.cards }</div>
+                <input type="text" onChange={ this.props.onAddInputChange }/>
+                <button onClick={ this.props.onAddClick }></button>
+            </div>
+        );
+    }
+});
 
 var Board = function() {
-    getInitialState: function() {
-    return {
-      clicked: false
-    };
-  },
-  onAddInputChange: function() {
 
-  },
-  onAddClick: function() {
-    this.setState( {
-      clicked: true
-    } );
-  },
   return (
     <div className="board">
       <div className="board-title">Board</div>
       <div>
         {[
-          <List title="List 1" cards={[ 
+            <ListContainer title="List 1" cards={[
             <Card text="Card 1" />,
             <Card text="Card 2" />,
             <Card text="Card 3" />
-          ]} />,
-          <List title="List 2" cards={[ 
-            <Card text="Card 1" />,
-            <Card text="Card 2" />,
-            <Card text="Card 3" />
-          ]} />,
-          <List title="List 3" cards={[ 
-            <Card text="Card 1" />,
-            <Card text="Card 2" />,
-            <Card text="Card 3" />
-          ]} /> 
+          ]} />
         ]}
       </div>
     </div>
